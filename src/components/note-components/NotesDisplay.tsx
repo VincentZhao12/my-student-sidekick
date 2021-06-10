6a5e070e120d5d60c405a86bdcc431950e7d86ae
VincentZhao12 <zhao.wentao.vincent@gmail.com>
2021-06-09 22:15:21 -0700

    Changed some note styling

--- src/components/note-components/NotesDisplay.tsx
+++ src/components/note-components/NotesDisplay.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@chakra-ui/button';
-import { VStack } from '@chakra-ui/layout';
+import { Box, VStack } from '@chakra-ui/layout';
 import React, { FC } from 'react';
+import './NoteDisplay.css';
 
 export interface NotesDisplayProps {
     onIndexChange?: (idx: number) => void;
@@ -14,22 +15,27 @@ const NotesDisplay: FC<NotesDisplayProps> = ({
     selectedIndex,
 }) => {
     return (
-        <VStack>
-            {noteTitles.map((note, index) => (
-                <Button
-                    onClick={() => {
-                        if (onIndexChange) onIndexChange(index);
-                    }}
-                    key={index}
-                    width={'3xs'}
-                    style={{ whiteSpace: 'normal', wordBreak: 'break-word' }}
-                    colorScheme={selectedIndex === index ? 'blue' : 'gray'}
-                    opacity={1}
-                >
-                    {note}
-                </Button>
-            ))}
-        </VStack>
+        <Box height="xl" overflow="scroll" className="customScroll">
+            <VStack>
+                {noteTitles.map((note, index) => (
+                    <Button
+                        onClick={() => {
+                            if (onIndexChange) onIndexChange(index);
+                        }}
+                        key={index}
+                        width={'3xs'}
+                        style={{
+                            whiteSpace: 'normal',
+                            wordBreak: 'break-word',
+                        }}
+                        colorScheme={selectedIndex === index ? 'blue' : 'gray'}
+                        opacity={1}
+                    >
+                        {note}
+                    </Button>
+                ))}
+            </VStack>
+        </Box>
     );
 };
 
