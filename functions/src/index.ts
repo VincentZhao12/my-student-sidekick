3c8782bbc318bbb48653cab63361eda3025cd1a5
VincentZhao12 <zhao.wentao.vincent@gmail.com>
2021-06-10 18:38:18 -0700

    changed website reading to incorporate metadata

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,25 +9,79 @@ import { JSDOM } from 'jsdom';
 //   response.send("Hello from Firebase!");
 // });
 
+interface CitationData {
+    title?: string;
+    url?: string;
+    siteName?: string;
+    date?: string;
+    publisher?: string;
+    author?: string | string[];
+}
+
 exports.fetchWebsiteInfo = functions.https.onCall(async (data, context) => {
     const { url } = data;
+    let foundData: CitationData = {};
 
     try {
         const response = await fetch(url);
         const text = await response.text();
         const dom = await new JSDOM(text);
 
-        let foundData: (String | null)[] = [];
+        dom.window.document.querySelectorAll('meta').forEach((meta) => {
+            const prop = meta.getAttribute('property');
+            const content = meta.content;
 
-        dom.window.document.querySelectorAll('script').forEach((element) => {
-            if (element.type === 'application/ld+json')
-                foundData = [...foundData, element.textContent];
+            if (prop === 'og:title') foundData.title = content;
+            else if (prop === 'og:url') foundData.url = content;
+            else if (prop === 'og:site_name') foundData.siteName = content;
+            else if (
+                prop === 'article:published_time' ||
+                meta.getAttribute('itemprop') === 'datePublished'
+            )
+                foundData.date = content;
+            else if (prop === 'article:publisher')
+                foundData.publisher = meta.content;
+            else if (
+                prop === 'article:author' ||
+                meta.getAttribute('name') === 'author'
+            )
+                foundData.author = content;
         });
-        return {
-            foundData,
-            text,
-        };
+
+        dom.window.document.querySelectorAll('script').forEach((script) => {
+            if (
+                script.type == 'application/ld+json' &&
+                script &&
+                script.textContent
+            ) {
+                let scriptData = JSON.parse(script.textContent);
+
+                if (Array.isArray(scriptData)) scriptData = scriptData[0];
+
+                if (scriptData['@graph']) scriptData = scriptData['@graph'][0];
+
+                const authors = scriptData.author;
+                if (!foundData.author) {
+                    if (Array.isArray(authors))
+                        foundData.author = authors.map((author) => author.name);
+                    else if (authors) foundData.author = authors.name;
+                }
+
+                if (
+                    scriptData['@type'] === 'Organization' ||
+                    scriptData['@type'] === 'NewsMediaOrganization'
+                )
+                    foundData.publisher = scriptData.name;
+
+                if (scriptData.publisher)
+                    foundData.publisher = scriptData.publisher.name;
+
+                if (!foundData.date && scriptData.datePublished)
+                    foundData.date = scriptData.datePublished;
+            }
+        });
     } catch (error) {
-        return error;
+        console.log(error);
     }
+    return foundData;
 });
--- src/pages/Bibliography.tsx
+++ src/pages/Bibliography.tsx
@@ -1,24 +1,61 @@
+import { Button } from '@chakra-ui/button';
+import { Input } from '@chakra-ui/input';
 import { Container } from '@chakra-ui/layout';
-import React, { FC, useEffect } from 'react';
+import React, { FC, useState } from 'react';
 import { functions } from '../firebase';
 
 export interface BibliographyProps {}
 
 const Bibliography: FC<BibliographyProps> = () => {
-    useEffect(() => {
+    const [author, setAuthor] = useState<string | string[]>('');
+    const [date, setDate] = useState<string>('');
+    const [title, setTitle] = useState<string>('');
+    const [publisher, setPublisher] = useState<string>('');
+    const [site, setSite] = useState<string>('');
+    const [url, setUrl] = useState<string>('');
+
+    const fetchCitationInfo = () => {
         const fetchWebsiteData = functions.httpsCallable('fetchWebsiteInfo');
-        const url =
-            'https://www.politico.com/news/2021/06/09/progressives-climate-provisions-infrastructure-492667';
 
         const fetchData = async () => {
             const response = await fetchWebsiteData({ url });
-            console.log(JSON.parse(response.data.foundData));
+
+            const data = response.data;
+
+            console.log(data);
+
+            let authors = data.author;
+
+            if (Array.isArray(authors))
+                setAuthor(authors.map((author) => author));
+            setAuthor((data && data.author) || '');
+            setDate((data && data.date) || '');
+            setTitle((data && data.title) || '');
+            setPublisher((data && data.publisher) || '');
+            setUrl(data && data.url);
+            setSite(data && data.siteName);
+
+            const thisDate = new Date(Date.parse(data.date));
+            console.log(thisDate.toDateString());
         };
 
         fetchData();
-    }, []);
+    };
 
-    return <Container></Container>;
+    return (
+        <Container>
+            <Input onChange={(e) => setUrl(e.target.value)} />
+            <Button onClick={fetchCitationInfo}>Cite</Button>
+            <p>Author: {author}</p>
+            <p>Date: {date}</p>
+            <p>Title: {title}</p>
+            <p>Publisher: {publisher} </p>
+            <p>Site Name: {site} </p>
+            <p>
+                Url: <a href={url}>{url}</a>
+            </p>
+        </Container>
+    );
 };
 
 export default Bibliography;
