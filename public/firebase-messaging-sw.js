b6c5c8477b42f0b8bce4010ad352ccde2ee876bd
VincentZhao12 <zhao.wentao.vincent@gmail.com>
2021-06-26 01:18:21 -0700

    hide api key and made leaked api key invalid

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,11 +2,8 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing the generated config
-var firebaseConfig = {
+const firebaseConfig = {
     messagingSenderId: '1081734645922',
-    projectId: 'my-student-sidekick',
-    apiKey: 'AIzaSyDj4CGLkkeGLzs0fjjTvm_IJDpmvFnNjKo',
-    appId: '1:1081734645922:web:adc96305eeb71596ca2ffb',
 };
 
 firebase.initializeApp(firebaseConfig);
