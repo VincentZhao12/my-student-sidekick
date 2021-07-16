022e5876fc07d54f155fa89670615d5a75cd095b
Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>
2021-07-16 01:03:35 -0700

    fixed some weird stuff

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing the generated config
+
+const firebaseConfig = {
+    messagingSenderId: '1081734645922',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
